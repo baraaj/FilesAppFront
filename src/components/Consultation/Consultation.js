9ec64441f7ade0598d5c98f90f067f69793528b8
Jridi Baraa <jeridibaraa@gmail.com>
2023-09-15 00:10:50 +0200

    last commit2

--- src/components/Consultation/Consultation.js
+++ src/components/Consultation/Consultation.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
 import { Navigate} from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
